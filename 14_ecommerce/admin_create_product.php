dbb4c52e5e71d1a16b9f88a9c7807d652d085d13
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-09-20 21:42:53 +0200

    create product edit

--- 14_ecommerce/admin_create_product.php
+++ 14_ecommerce/admin_create_product.php
@@ -1,3 +1,4 @@
+<?php require_once 'maininclude.inc.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +11,48 @@
     <main>
         <h1>Produkt erstellen</h1>
         <?php include 'showerrors.inc.php'; ?>
-        Bei File-Uploads IMMER enctype="multipart/form-data"
+        <!-- Bei File-Uploads IMMER enctype="multipart/form-data" -->
         <form action="admin_create_product.php" method="POST" enctype="multipart/form-data">
+            <label >SKU (Artikelnummer)</label><br>
+            <input type="text" name="sku"><br>
+            <label >Brand</label><br>
+            <select name="brand_id">
+                <?php 
+                // alle Brands fuer dieses SELECT laden
+                $brands = $dba->getBrands();
+                foreach($brands as $b){
+                    echo '<option value="'.$b->id.'">'.htmlspecialchars($b->name).'</option>';
+                }
+                ?>
+            </select>
 
+            <label>Category</label>
+            <select name="category_id">
+                <option value=""></option>
+                <?php
+                    $categories = $dba->getCategories();
+                    foreach($categories as $c){
+                        echo '<option value="'.$c->id.'">'.htmlspecialchars($c->name).'</option>'; 
+                    }
+                ?>
+            </select><br>
+
+            <label>Name</label><br>
+            <input type="text" name="name"><br>
+            <label>Descrition</label><br>
+            <textarea name="descpription"></textarea><br>
+            <label>Picture</label>
+            <input type="file" name="picture"><br>
+            <label>Price</label>
+            <input type="text" name="price"><br>
+            <label>Stock</label>
+            <input type="text" name="stock"><br>
+
+            <button name="bt_create_product">Speichern</button>
         </form>
     </main>
 </body>
 </html>
+
+
+<!-- TODO: Category einfuegen und Editen -->
