d3eec568d568c7f4eb3daac881ee63e25819fdd8
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-09-27 20:24:29 +0200

    session cart array iterieren

--- 14_ecommerce/cart.php
+++ 14_ecommerce/cart.php
@@ -36,10 +36,56 @@ if(isset($_POST['bt_add_to_cart'])){
 <body>
     <?php include 'header.inc.php';?>
     <main>
-        <h1>Willkommen!</h1>
+        <h1>Warenkorb</h1>
         <?php
             include 'showerrors.inc.php';
+
         ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>SKU</th>
+                    <th>Name</th>
+                    <th>Bild</th>
+                    <th>Einzelpreis</th>
+                    <th>Menge</th>
+                    <th>Gesamt</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                $total = 0;
+
+
+                // Iterieren ueber das Session-Cart-Array
+                foreach($_SESSION['cart'] as $entry){
+                    $productId = $entry['product_id'];
+                    $qty = $entry['qty'];
+
+                    $product = $dba->getProductById($productId);
+                    if($product == FALSE){
+                        continue; // wenn das Produkt nicht mehr verfuegbar ist
+                    }
+                    echo '<tr>'; // Zeile
+                    echo '<td>'.htmlspecialchars($product->sku).'</td>'; // Spalte
+                    echo '<td>'.htmlspecialchars($product->name).'</td>';
+                    echo '<td><img src="'.$product->picture.'" style="max-height: 60px;"></td>';
+
+                    $unitPrice = $product->price;
+                    $productTotalPrice = $unitPrice * $qty;
+                    $total += $productTotalPrice;
+                    echo '<td>'.$unitPrice.' EUR</td>';
+                    echo '<td>'.$qty.' Stueck</td>';
+                    echo '<td>'.$productTotalPrice.' EUR</td>';
+                    echo '</tr>';                    
+                }
+                ?>
+            </tbody>
+        </table>
+        <p>
+            Gesamtsumme: <strong><?php echo $total; ?> EUR.</strong>
+        </p>
     </main>
 </body>
 </html>
