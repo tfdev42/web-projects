7d5c7063930075bf373de575e6cc61c660b48af8
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-10-01 18:39:06 +0200

    get

---
+++ cWDC3/get/index.php
@@ -0,0 +1,45 @@
+<?php
+    // show errors
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+    if(isset(is_numeric($_GET['number']))
+    && $_GET['number'] > 0
+    && $_GET['number'] == round($_GET['number'], 0)){
+        $theNumber = $_GET['number'];
+        $i = 2;
+        $isPrime = true;
+
+        while($i < $theNumber){
+            if($theNumber % $i == 0){
+                $isPrime = false;
+            }
+            $i++;
+        }
+
+        if($isPrime){
+            echo '<p>' . $theNumber . 'is a prime number.</p>';
+        } else {
+            echo '<p>' . $theNumber . 'is NOT a prime number.</p>';
+        }
+    } elseif($_GET){
+        echo "<p>Please enter a positive shole number.</p>"
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <input type="text" >
+        <input type="submit" value="Go!">
+    </form>
+</body>
+</html>
