8d2ab51a68494fd3d74c9b91db08afff70fb1e1d
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-10-03 22:32:53 +0200

    notes - FormValidator

---
+++ notes.php
@@ -0,0 +1,41 @@
+<?php
+class FormValidator {
+    private $data;
+    private $errors = [];
+
+    public function __construct($data) {
+        $this->data = $data;
+    }
+
+    public function validateRequired($field, $label) {
+        if (empty($this->data[$field])) {
+            $this->addError($field, "$label is required.");
+        }
+    }
+
+    public function validateEmail($field, $label) {
+        if (!filter_var($this->data[$field], FILTER_VALIDATE_EMAIL)) {
+            $this->addError($field, "$label is not a valid email address.");
+        }
+    }
+
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    private function addError($field, $message) {
+        $this->errors[$field] = $message;
+    }
+}
+
+// Example usage:
+$data = $_POST;
+$validator = new FormValidator($data);
+$validator->validateRequired('email', 'Email');
+$validator->validateEmail('email', 'Email');
+
+$errors = $validator->getErrors();
+if (!empty($errors)) {
+    // Handle validation errors
+}
+?>
