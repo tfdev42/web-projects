39f49f69293bb93d10a30e1fc59992fe3e02bbe2
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-10-05 22:00:52 +0200

    boot ddl sql

--- bfi/16_insurance_portal/db/ddl.sql
+++ bfi/16_insurance_portal/db/ddl.sql
@@ -3,4 +3,41 @@
 -- product(ID(PK), name, description, pricePerMinute)
 -- insurance(ID(PK), CustomerId(FK), productId(FK), status, start, end, comment, kennzeichen)
 
-CREATE DATABASE IF NOT EXISTS 
+CREATE DATABASE IF NOT EXISTS 2023105_boot;
+USE 2023105_boot;
+
+CREATE TABLE user(
+    id INT AUTO_INCREMENT,
+    fname VARCHAR(200) NOT NULL,
+    lname VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    street VARCHAR(200) NOT NULL,
+    city VARCHAR(200) NOT NULL,
+    country VARCHAR(200) NOT NULL,
+    zip VARCHAR(200) NOT NULL,
+    paymentMethod VARCHAR(200),
+    password VARCHAR(100) NOT NULL,
+    is_admin TINYINT(1),
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
+);
+
+CREATE TABLE product(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    description VARCHAR(500),
+    pricePerMinute DECIMAL(4,2),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE insurance(
+    id INT AUTO_INCREMENT,
+    status VARCHAR(100),
+    startTime DATETIME,
+    endTime DATETIME,
+    comment VARCHAR(500),
+    licensePlate VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (customerId) REFERENCES user(id),
+    FOREIGN KEY (productId) REFERENCES product(id),
+);
