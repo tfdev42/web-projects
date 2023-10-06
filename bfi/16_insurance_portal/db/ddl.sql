aa375f934391ae9d5de3d95bb082af591cd33d02
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-10-06 20:20:50 +0200

    sql + models

--- bfi/16_insurance_portal/db/ddl.sql
+++ bfi/16_insurance_portal/db/ddl.sql
@@ -16,7 +16,7 @@ CREATE TABLE user(
     city VARCHAR(200) NOT NULL,
     country VARCHAR(200) NOT NULL,
     zip VARCHAR(200) NOT NULL,
-    paymentMethod VARCHAR(200),    
+    paymentMethod VARCHAR(200) NULL,    
     is_admin TINYINT(1),
     PRIMARY KEY (id),
     UNIQUE KEY (email)
@@ -32,12 +32,14 @@ CREATE TABLE product(
 
 CREATE TABLE insurance(
     id INT AUTO_INCREMENT,
+    customer_id INT,
+    product_id INT,
     status VARCHAR(100),
     startTime DATETIME,
     endTime DATETIME,
     comment VARCHAR(500),
     licensePlate VARCHAR(255),
     PRIMARY KEY (id),
-    FOREIGN KEY (customerId) REFERENCES user(id),
-    FOREIGN KEY (productId) REFERENCES product(id)
+    FOREIGN KEY (customer_id) REFERENCES user(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
---
+++ bfi/16_insurance_portal/db/models.inc.php
@@ -0,0 +1,17 @@
+<?php
+class User {
+    public int $id;
+    public string $fname;
+    public string $lname;
+    public string $email;
+    public string $password;
+    public string $street;
+    public string $city;
+    public string $country;
+    public string $zip;
+    public string $paymentMethod;
+    public bool $is_admin = false;
+}
+
+
+?>
