4372e2c8e07f4fa98a4f802e9daac962edc1e056
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-10-06 20:41:20 +0200

    models

--- bfi/16_insurance_portal/db/models.inc.php
+++ bfi/16_insurance_portal/db/models.inc.php
@@ -1,6 +1,6 @@
 <?php
 class User {
-    public int $id;
+    public int $userId;
     public string $fname;
     public string $lname;
     public string $email;
@@ -11,6 +11,50 @@ class User {
     public string $zip;
     public string $paymentMethod;
     public bool $is_admin = false;
+    public function __construct(
+        int $userId,
+        string $fname,
+        string $lname,
+        string $email,
+        string $password,
+        string $street,
+        string $city,
+        string $country,
+        string $zip,
+        string $paymentMethod,
+        bool $is_admin = false
+    ) {
+        $this->userId = $userId;
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->email = $email;
+        $this->password = $password;
+        $this->street = $street;
+        $this->city = $city;
+        $this->country = $country;
+        $this->zip = $zip;
+        $this->paymentMethod = $paymentMethod;
+        $this->is_admin = $is_admin;
+    }
+}
+
+
+class Product {
+    public int $productId;
+    public string $productName;
+    public string $productDescription;
+    public float $pricePerMinute;
+}
+
+class Insurance {
+    public int $insureId;
+    public int $customerId;
+    public int $productId;
+    public string $status;
+    public DateTime $startTime;
+    public DateTime $endTime;
+    public ?string $comment;
+    public string $licensePlate;
 }
 
 
