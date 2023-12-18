ca8444946382fe55b571cc6b8db377e830ef42c4
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-18 12:31:08 +0100

    login.php

--- other/signupAndLogin/login.php
+++ other/signupAndLogin/login.php
@@ -10,6 +10,15 @@
     />
   </head>
   <body>
-    
+    <form method="post">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email">
+
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password">
+
+      <button type="submit">Submit</button>
+
+    </form>
   </body>
 </html>
