48b45227c23db6136efd8f37c87a06b05e762a79
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-18 13:17:47 +0100

    login.php >> php code

--- other/signupAndLogin/login.php
+++ other/signupAndLogin/login.php
@@ -1,3 +1,33 @@
+<?php
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+  $mysqli = require __DIR__ . "/database.php";
+
+  $sql = sprintf("SELECT * FROM user
+                  WHERE email = '%s'",
+                  $mysqli->real_escape_string($_POST["email"])); // to avoid SQL injection, Escape special characters, if any
+
+  $result = $mysqli->query($sql);
+
+  $user = $result->fetch_assoc();
+  // var_dump($user);
+  // exit;
+
+  // if a record was found >> check password
+  if ($user) {
+    if (password_verify($_POST["password"], $user["password_hash"])){
+      die("Login successfull!");
+    }
+  }
+
+  $is_invalid = true; // set also in html body
+
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,9 +40,15 @@
     />
   </head>
   <body>
+    <h1>Login</h1>
+
+    <?php if ($is_invalid): ?>
+        <em>Invalid login</em>
+    <?php endif; ?>
     <form method="post">
       <label for="email">Email</label>
-      <input type="email" name="email" id="email">
+      <input type="email" name="email" id="email"
+              value="<?php echo htmlspecialchars($_POST["email"] ?? "") ?>">
 
       <label for="password">Password</label>
       <input type="password" name="password" id="password">
