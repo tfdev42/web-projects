8f1279849c66144d278937a55038844cfaec6c99
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-20 12:51:24 +0100

    signupLogin w/ PDO - signup.php

---
+++ other/signupLoginPDO/signup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+
+    <form action="" method="post" novalidate>
+        <div>
+            <label for="name">Name</label>
+            <input id="name" type="text" placeholder="Name">
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input id="name" type="text" placeholder="email@example.com">
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input id="password" type="text" placeholder="Password123!">
+        </div>
+
+        <div>
+            <label for="password_confirm">Confirm Password</label>
+            <input id="password_confirm" type="text" placeholder="Repeat password">
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+    
+</body>
+</html>
