6ac3049000c57c35b3de0820cb21215c65eb0002
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-20 13:02:36 +0100

    main.inc.php

---
+++ other/signupLoginPDO/inc/main.inc.php
@@ -0,0 +1,9 @@
+<?php
+// show errors in linux
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// always start session
+session_start();
+
+$errors = [];
--- other/signupLoginPDO/signup.php
+++ other/signupLoginPDO/signup.php
@@ -8,7 +8,7 @@
 <body>
     <h1>Sign Up</h1>
 
-    <form action="" method="post" novalidate>
+    <form id="signup" action="" method="post" novalidate>
         <div>
             <label for="name">Name</label>
             <input id="name" type="text" placeholder="Name">
@@ -21,12 +21,12 @@
 
         <div>
             <label for="password">Password</label>
-            <input id="password" type="text" placeholder="Password123!">
+            <input id="password" type="password" placeholder="Password123!">
         </div>
 
         <div>
             <label for="password_confirm">Confirm Password</label>
-            <input id="password_confirm" type="text" placeholder="Repeat password">
+            <input id="password_confirm" type="password" placeholder="Repeat password">
         </div>
         <div>
             <button type="submit">Submit</button>
