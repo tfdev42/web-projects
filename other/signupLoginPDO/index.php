daa2c28983d3cb2f08111567ff32470156a7d182
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-23 11:38:23 +0100

    header inc

---
+++ other/signupLoginPDO/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once "main.inc.php";
+require_once "./inc/header.inc.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <main>
+        <h1>Welcome</h1>
+        <p>
+            
+        </p>
+    </main>
+    
+</body>
+</html>
