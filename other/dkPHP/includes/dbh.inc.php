d6855f6f319f9a86547372e24a1a66337a0c9c1d
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-27 09:48:42 +0100

    db handler

---
+++ other/dkPHP/includes/dbh.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=20231226_dk_db";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (\PDOException $e) {
+
+    echo "Connection failed: " . $e->getMessage();
+
+}
