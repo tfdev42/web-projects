74163e1d9f27fbfe66c2b270ae9c95506f43c7a7
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-27 11:36:02 +0100

    Change Account index.php - Update and Delete user

--- dk_proj/index.php
+++ dk_proj/index.php
@@ -3,17 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup</title>
+    <title>Change Account</title>
 </head>
 <body>
     
-    <h3>Signup</h3>
+    <h3>Change account</h3>
 
-    <form action="includes/formhandler.inc.php" method="post">
+    <form action="includes/userupdate.inc.php" method="post">
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="pwd" placeholder="Password">
         <input type="text" name="email" placeholder="E-mail">
-        <button>Signup</button>
+        <button>Update</button>
+    </form>
+
+    <h3>Delete account</h3>
+
+    <form action="includes/userdelete.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Update</button>
     </form>
 </body>
 </html>
