f6f267b40042526669b7b1b9ec2f94e01dd8307a
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-27 18:56:16 +0100

    search

--- dk_proj/index.php
+++ dk_proj/index.php
@@ -6,6 +6,15 @@
     <title>Change Account</title>
 </head>
 <body>
+
+    <h3>Signup</h3>
+
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button>Signup</button>
+    </form>
     
     <h3>Change account</h3>
 
@@ -22,6 +31,14 @@
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="pwd" placeholder="Password">
         <button>Update</button>
+    </form>
+
+    <h3>Search</h3>
+
+    <form class="searchform" action="search.php" method="post">
+        <label for="search">Search for user</label>
+        <input id="search" type="text" name="usersearch" placeholder="Search...">
+        <button>Search</button>
     </form>
 </body>
 </html>
