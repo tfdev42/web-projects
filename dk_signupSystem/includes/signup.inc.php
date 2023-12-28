949b109cfbb68ae50d57e9e8969081ed5495b768
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-28 10:58:20 +0100

    preparing mvc files

---
+++ dk_signupSystem/includes/signup.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        
+        require_once "dbh.inc.php";
+
+        $dba = 
+    } catch (PDOException $e) {
+
+        die("Query failed: " . $e->getMessage());
+        
+    }
+
+} else {
+    header("Location: ../index.php");
+    die();
+}
