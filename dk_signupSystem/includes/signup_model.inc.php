f3b60e2af568b533b659f70b730dd801be34d054
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-28 11:09:35 +0100

    declare(strict_types=1) into mvc files

---
+++ dk_signupSystem/includes/signup_model.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+// 'MODEL' IS ONLY FOR INTERACTING WITH THE DATABASE
+// SENSITIVE FUNCTIONS!
+
+// WE ARE ALLOWING OUR CODE TO HAVE TYPE DECLARATIONS
+// The primary purpose of type declarations is to enhance code clarity, catch errors early, and improve maintainability.
+
+declare(strict_types=1);
+// THIS GOES INTO CONTR AND VIEW TOO!
