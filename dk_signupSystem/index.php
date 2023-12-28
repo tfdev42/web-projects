707185369564537c9632ac98f595cbbe92b18edd
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-28 12:59:24 +0100

    signup view errors

--- dk_signupSystem/index.php
+++ dk_signupSystem/index.php
@@ -1,3 +1,10 @@
+<?php
+require_once 'includes/config_session.inc.php';
+require_once 'includes/signup_view.inc.php';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +20,7 @@
         <form action="includes/login.inc.php" method="post">
             <input type="text" name="username" placeholder="Username">
             <input type="password" name="pwd" placeholder="Password">
+            <button>Login</button>
         </form>
 
         <h3>Signup</h3>
@@ -23,6 +31,12 @@
             <input type="text" name="email" placeholder="E-Mail">
             <button>Signup</button>
         </form>
+
+        <?php
+        check_signup_errors();
+        // VIEW model handels this
+        
+        ?>
     </main>
 </body>
 </html>
