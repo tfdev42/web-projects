e7a1d2780a81dda8a076a90eecea06db0a9866b4
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-12-28 10:43:07 +0100

    session and cookie configs

---
+++ dk_signupSystem/includes/config_session.inc.php
@@ -0,0 +1,32 @@
+<?php
+// mandatory security
+ini_set('session.use_only_cookies', 1);
+ini_set('session.use_strict_mode', 1);
+
+
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true
+]);
+
+session_start();
+
+// regenerate session ID to make it better than the default
+// if there is no session id set >> set one
+if( ! isset($_SESSION["last_regeneration"])){
+    regenerate_session_id();
+} else {
+    $interval = 60 * 30;
+    // last_regeneration older than 30 minutes? >> set new
+    if(time() - $_SESSION["last_regeneration"] >= $interval){
+        regenerate_session_id();
+    }
+}
+
+function regenerate_session_id(){
+    session_regenerate_id();
+    $_SESSION["last_regeneration"] = time();
+}
