afe84acaccef049896a35092d8fb7790f1e238cc
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-01-01 19:43:56 +0100

    config_session

---
+++ mvc_insurance/includes/config_session.inc.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('session.use_only_cookies', 1);
+ini_set('session.use_strict_mode', 1);
+
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true
+]);
+
+session_start();
+
+$interval = 60 * 30;
+
+if( ! isset($_SESSION["last_regeneration"])) {
+    regenerate_session_id();
+} elseif ( (time() - $_SESSION["last_regeneration"]) >= $interval){
+    regenerate_session_id();
+}
+
+function regenerate_session_id(){
+    session_regenerate_id(true);
+    $_SESSION["last_regeneration"] = time();
+}
--- mvc_insurance/index.php
+++ mvc_insurance/index.php
@@ -1,5 +1,5 @@
 <?php
-// require_once "./includes/dbh.inc.php";
+require_once "./includes/config_session.inc.php";
 ?>
 
 
