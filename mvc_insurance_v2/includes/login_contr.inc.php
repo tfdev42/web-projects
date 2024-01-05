8de6edbcc965dd51228eb80dfb3be10269217b1b
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-01-05 13:51:07 +0100

    login pw/uname check functions

--- mvc_insurance_v2/includes/login_contr.inc.php
+++ mvc_insurance_v2/includes/login_contr.inc.php
@@ -1,1 +1,19 @@
 <?php
+declare(strict_types=1);
+
+
+function is_password_wrong(string $pwd, string $hashedPwd) : bool {
+    if ( ! password_verify($pwd, $hashedPwd)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function is_username_wrong(bool|array $result) : bool{
+    if (! $result){
+        return true;
+    } else {
+        return false;
+    }
+}
--- mvc_insurance_v2/includes/login_model.inc.php
+++ mvc_insurance_v2/includes/login_model.inc.php
@@ -1,2 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+function get_user(object $pdo, int $userId) : array | false {
+    $query =
+    "SELECT *
+    FROM users
+    WHERE id=:userId;";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":userId", $userId);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
