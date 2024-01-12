231603fa4c399d8f25385ea74165f18912102033
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-01-12 22:55:10 +0100

    Form class

--- mvc_insurance_v3/Classes/Form.php
+++ mvc_insurance_v3/Classes/Form.php
@@ -1,5 +1,31 @@
 <?php
+declare(strict_types=1);
 
 class Form{
-    
+    public $formData = [];
+
+    public function __construct() {
+        $this->isInputEmpty($this->getPOSTasArray());
+    }
+
+    function getPOSTasArray() {
+        $post = $_POST;
+        $result = [];
+        foreach($post as $key => $value){
+            if($key === "hidden" || $value === "bt_signup"){
+                continue;
+            }
+            $result[$key] = $value;
+        }
+        return $result;
+    }
+
+    function isInputEmpty(array $formData) {
+        $this->formData = $formData;
+        foreach($formData as $key => $value){
+            if(empty($value)){
+                $errors[] = "Fill in all fields!";
+            }
+        }
+    }
 }
--- mvc_insurance_v3/includes/signup_view.inc.php
+++ mvc_insurance_v3/includes/signup_view.inc.php
@@ -1,2 +1,7 @@
 <?php
 
+function displaySignuUp() { ?>
+    <form action="./signup_contr.inc.php" method="post">
+        
+    </form>
+<?php }
