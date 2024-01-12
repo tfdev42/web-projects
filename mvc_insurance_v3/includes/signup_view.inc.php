fa6dd0bf51b8882881829d4930545c232dde36d2
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-01-12 23:01:48 +0100

    showSignupErrors

--- mvc_insurance_v3/includes/signup_view.inc.php
+++ mvc_insurance_v3/includes/signup_view.inc.php
@@ -1,7 +1,20 @@
 <?php
 
+function showSignupErrors() {
+    if($_SESSION["signup_errors"]){
+        $errors = $_SESSION["signup_errors"];
+        foreach($errors as $e){
+            echo '<p class="error">';
+            echo htmlspecialchars($e);
+            echo '</p><br>';
+        }
+        $_SESSION["signup_errors"] = null;
+        $errors = null;
+    }
+}
+
 function displaySignuUp() { ?>
     <form action="./signup_contr.inc.php" method="post">
-        
+
     </form>
 <?php }
