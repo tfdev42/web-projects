ef5c11d644a2c30a91c25c0b1e8933764e2f6142
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-01-20 19:47:38 +0100

    SIgnupContr Methods

--- OOP_Login_System/Classes/SignupContr.class.php
+++ OOP_Login_System/Classes/SignupContr.class.php
@@ -14,5 +14,32 @@ class SignupContr {
         $this->email = $email;
     }
 
+    /**
+     * Returns TRUE if an input is empty
+     */
+    private function isInputEmpty() {
+        $result = true;
+        if(empty($this->uid)
+        || empty($this->pwd)
+        || empty($this->pwd_repeat)
+        || empty($this->email)){
+            return $result;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    /**
+     * Return TRUE if Username contains NOT only [a-zA-Z0-9]
+     */
+    private function isUidInvalid() {
+        $result = true;
+        if (preg_match("/^[a-zA-Z0-9]*$/", $this->uid)){
+            $result = false;
+        }
+        return $result;
+    }
+
 
 }
