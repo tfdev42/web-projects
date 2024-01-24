863b72349cd89b6aae1e0c8d1a6dc2177afa330f
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-01-24 17:06:48 +0100

    .

--- OOP_MVC_Template/Classes/Test.class.php
+++ OOP_MVC_Template/Classes/Test.class.php
@@ -2,8 +2,59 @@
 
 class Test extends Dbh {
 
-    public function getUsers() {
+    public function getUsersDataWithTableNames() {
+        $query = "SELECT * FROM users;";
         
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute();
+
+        while($row = $stmt->fetch()){
+            foreach ($row as $k=>$v){
+                echo $k . " = " . $v . " ";
+                echo "<br>";
+            }
+        }
+        $stmt=null;
+
+    }
+
+    public function getUsers() {
+        $query = "SELECT users_firstname, user_dateofbirth FROM users;";
+
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute();
+
+        while($row = $stmt->fetch()){
+            echo $row["users_firstname"] . ", DoB: " .$row["user_dateofbirth"] . "<br>";
+        }
+        $stmt=null;
+    }
+
+    public function setUser($firstName, $lastName, $dob){
+        $query=
+        "INSERT INTO users (users_firstname, users_lastname, user_dateofbirth)
+        VALUES (:firstName, :lastName, :dob);";
+
+        $stmt = $this->connect()->prepare($query);
+        $stmt->bindValue(":firstName", $firstName, PDO::PARAM_STR);
+        $stmt->bindValue(":lastName", $lastName, PDO::PARAM_STR);
+        $stmt->bindValue(":dob", $dob, PDO::PARAM_STR);
+        $result = $stmt->execute();
+
+        if ($result){
+            echo "OK";
+        } else echo "NOK";
+        $stmt=null;
+    }
+
+    public function updateLastname($firstName, $newLastName) {
+        $query=
+        "UPDATE users
+        SET users_lastname = :newLastName
+        WHERE users_firstname = :firstName;";
+
+        $stmt = $this->connect()->prepare($query);
+        // TODO
     }
 
 }
---
+++ OOP_MVC_Template/index.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+include_once "./includes/autoload_classes.inc.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <?php
+    $test = new Test();
+    $test->getUsersDataWithTableNames();
+    ?>
+    <p>getUsers function <br>
+        <?php
+        $test->getUsers();
+        ?>
+    </p>
+    <p>Set User <br>
+        <?php
+            $test->setUser("Pipi", "Langstrumpf", "2004-03-28");
+        ?>
+    </p>
+</body>
+</html>
